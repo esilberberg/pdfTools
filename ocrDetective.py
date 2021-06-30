9a06cf64a61642e8e03715975edd2574bfe2e621
esilberberg <72359394+esilberberg@users.noreply.github.com>
2021-06-30 14:53:58 -0400

    first commit

---
+++ ocrDetective.py
@@ -0,0 +1,34 @@
+import subprocess
+from sys import stderr
+import os
+from datetime import datetime
+
+path = input('Copy and past the directory ')
+
+now = datetime.now()
+date_time = now.strftime("%Y-%m-%d at %H:%M:%S")
+start_message = f'+++ As of {date_time} ocrDetective suspects these files do not contain OCR +++'
+
+suspects = []
+suspects.append(start_message)
+
+for root, dirs, files in os.walk(path, topdown=False):
+    for name in files:
+        filepath = os.path.join(root, name)
+        with subprocess.Popen(["pdffonts", filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+            out = proc.stdout.readlines()
+            out_error = proc.stderr.readlines()
+            font_count = len(out)
+
+        if font_count - 2 == 0:
+            print(filepath)
+            suspects.append(filepath)
+        else:
+            pass
+
+output_txt = os.path.join(path, "suspects.txt")
+
+print(suspects)
+with open(output_txt, "w") as f:
+    for suspect in suspects:
+        f.write("%s\n" % suspect)
